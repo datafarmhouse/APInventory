bb9880ecf5eabe8e35203064a8a8b3d232501bfa
Geert van Leemputten <geert.van.leemputten@gmail.com>
2023-07-16 13:13:22 +0200

    0.0.1

---
+++ src/test/java/be/datafarmhouse/apinventory/ApInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package be.datafarmhouse.apinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
